9d155ad2410f9a9e4f56f2e51198d7d16aa16bae
Chaitra K R <chaitrarkannavar@gmail.com>
2026-09-16 11:11:10 +0530

    Add Cure Health test case to demonstrate appointment booking flow

---
+++ tests/Task/cureHealth.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+
+test("Cure Health", async ({ page }) => {
+  await page.goto("https://katalon-demo-cura.herokuapp.com/", {
+    waitUntil: "load",
+  });
+  await page.locator("#btn-make-appointment").click();
+  await page.locator("#txt-username").fill("John Doe");
+  await page.locator("#txt-password").fill("ThisIsNotAPassword");
+  await page.locator("#btn-login").click();
+  await expect(page.locator(".col-sm-12.text-center")).toHaveText(
+    "Make Appointment",
+  );
+});
